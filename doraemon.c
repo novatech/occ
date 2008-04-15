bdc99353beececb61fc4cabb9aa6a70d263234b1
novatech <novatech@users.noreply.github.com>
2008-04-16 03:31:54 +0800

    doraemon

---
+++ doraemon.c
@@ -0,0 +1,33 @@
+/* ***********************************
+/* print modified doraemon song lyrics
+/************************************/
+#include <stdio.h>
+#define O_o "llllllllllllllllIlIllllIlIIlllllllllllllllllllIlllllllllllllllllllllIlIllllllllllllllllllllIlllllllllllllllllllllIIl\
+lllllllIlIllllllllllllllllllIlllllllllIIllllllllllllllIlllllllllllllllIllIlllllllllIlllllll\
+lllllllllllllIlIIllllllllllIlIllllllllllllIlIllllllllllllllIIllllllllllIlIllllllllllllIlIllllllllllllllI\
+|llllIlllllllllIlIIllllllllllllllllllllIll\
+lllIllllllllllllllllllIllllllllllllllIlIlllllllllllllIllllllllllllllllIlIlllllllllll\
+IIlllllllllllllllllllIlllllIlllllIlllllllllllIlllllllllllllllIllllllllllllllllllIIlllllllllllllIlIlllllllllllIllllllllIllllllllllllIlllllllll\
+llllllllllllIlllllllllllIIllIlllllIllllllllllllllllllIlllllllllllllllllllllllIlIllllllllllllllllllIllllllllllllllIlIIllIlllllllllIllllllllllllllllllIlllllllll\
+llllllllllllI|lllllllllllllIlllllllllllllllllllllIlllllllllllIlIIl\
+lllIlllllllllIlIIllIlllllllllllllllllllllIllllllllllllIlIllllllllllllllllllllIIllIllllllllllllllllll\
+lllIllllllllllllIlIllllllllllllllllllllI|lllllllllllllllllllIlllllllllllllllllllllIlllllllllllIlIIlllllllllllllIlIlllllllllllIlIllllllllllllllIIlllllllllllIlllllllllllllllllllllIllllll\
+lllIllllllllIIllllIlllllllllllllllIllllllllllllllIlIllllllllllllllllllllI|llllllllllllIlIllllllllllllIlllllllllllllllllllllIIllllllllllllllIlllllllllllllllIllIlllllllllIllllllllllllllllllllIlIIllIlllll\
+IllllllllllllllllllIllllllllllllllllllllIlIllllllllllllllIlllllllllllllllllllllllllIlI|lllllllllllllIlIlllllllllllIllllllllIllllllllllllIlllllllllllllllllllllIlllllllllllIIlllllllllIllllllllllllllllllllIllllllllllllllllll\
+lllIIllIlllllIllllllllllllllllllIllllllllllllllIlIlllllllllllllIlI|lIlIlIllllllllllllllIIlIlIllllllllllllllIIlIl\
+lllllllllllllIIII|llllIlllllllllllllllIllllllllllllllllllIlIlllllIlllllllllllllIlllllllllllllllIllllllllllllllIllllllllllllllIllllllllllllllIllllllllllllllIII"
+#define L y,t
+#define W 10*9+6
+#define _ t[x]
+#define X y = W
+#define o_o int main(x,L) char t[]; { t=
+#define o_O ;X;for\
+(x = 0; x<=(19.5\
+*W);x++){\
+(_ != 'l') ? pri\
+ntf("%c",\
+((_ == '|') ? '\n':(y%9\
+6) ? y : 32)) :y+\
++ ;(_ == 'I') ? X :\
+0;}return 0;}
+o_o O_o o_O
